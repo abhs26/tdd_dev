970ccef576bfa5ec5be807028502b1f983171990
abhishek <abhs26@gmail.com>
2017-10-23 22:10:29 +0100

    added functional test description

--- tdd_dev/users/tests/functional_tests.py
+++ tdd_dev/users/tests/functional_tests.py
@@ -4,4 +4,27 @@ browser = webdriver.Firefox()
 browser.get('http://127.0.0.1:8000')
 
 print(browser.title)
+
+#User has heard about our webpage. User
+#go to the website through the webbrowser
+browser.get('http://127.0.0.1:8000')
+
+#When page opens user see tdd_dev in title
 assert 'tdd_dev' in browser.title
+
+
+#user is interested to signup
+#user clicks on the signup button
+
+#new page opens
+#User see all the fields
+#email , username, password, retype password and signup button
+
+
+#User Enters the fields value
+
+
+#user enter the submit
+
+#user see the email confirmation mail
+
